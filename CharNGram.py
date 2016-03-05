c28aff27307200f4f95d43bbad18144c424ee490
Wally Guzman <gualbertoguzman@gmail.com>
2016-03-05 14:09:53 -0600

    Completed getConditionalCounts function

--- CharNGram.py
+++ CharNGram.py
@@ -2,11 +2,13 @@
 
 """
 Splits string into character n-grams of length n
+
  @param string the training data
  @param n the n-gram length
+ @return list of ngrams
 """
 def getNGrams(sentence, n):
-  sentence = (" " * (n - 1)) + sentence + " "   # Append start and end spaces
+  sentence = (" " * (n - 1)) + sentence + " "
   return [string[i:i+n] for i in range(len(sentence) - n + 1)]
 
 
@@ -15,9 +17,10 @@ def getNGrams(sentence, n):
     @param sentences a list of sentences from the training data
     @param n the length of an n-gram
     @return a mapping of context (unique substrings of the first n-1 characters) to endings (last character) and their frequencies
-"""
-def getConditionalCounts(sentences, n):
-  """ for each sentence in list of sentences:
+      a dictionary of mapping between the first n - 1 characters of an ngram and
+      a list of tuples of the ending characters
+
+  for each sentence in list of sentences:
         get n-grams using above method
         group by first n-1 characters (context)
         for each unique context, create a map of last character counts
@@ -27,11 +30,19 @@ def getConditionalCounts(sentences, n):
           e.g., your CFD  would have lots of entries that look like: 
                 "chai" -> ["r" -> 1, "n -> 1]
                 as an key-value pair.
-        
-
-      (NOTE: you don't have to structure your conditional frequency distribution in this particular way; I just found it more intuitive.
-      If it doesn't make sense I can go over it on gchat, or feel free to come up with your own implementation if another way makes more sense!)
 """
+def getConditionalCounts(sentences, n):
+  freqCount = {}
+  for sentence in sentences:
+    ngrams = getNGrams(sentence, n)
+    for gram in ngrams:
+      context = gram[:n - 1]
+      if not context in freqCount:
+        freqCount[context] = [(gram[-1], 1)]
+      else:
+        freqCount[context].append((gram[-1], 1))
+  return freqCount
+
 
 class CharNGram:
   def __init__(self, name, conditionalCounts, n):

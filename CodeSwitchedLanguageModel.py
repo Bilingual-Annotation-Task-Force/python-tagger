99328002379b3f20ddbdd46566a148b3f201e2ba
Wally Guzman <gualbertoguzman@gmail.com>
2016-03-24 19:09:35 -0500

    Translating functional Scala calls to Python

--- CodeSwitchedLanguageModel.py
+++ CodeSwitchedLanguageModel.py
@@ -8,38 +8,9 @@ class codeSwitchedLanguageModel:
     self.models = models
 
   def guess(self, word):
-    highestProb = 0.0
-    lowerWord = word.toLower()
-
-    # Get maximum NGram probability over all models
-    for model in self.models:
-      modelProb = model.ngramProb(lowerWord)
-      highestProb = max(highestProb, modelProb)
-
-    # Find model with highest probability
-    guess = []
-    for model in self.models:
-      if model.ngramProb(lowerWord) == highestProb:
-        guess.append(model)
-
-    # Functional calls in Python?
-    # highestProb = max([model.ngramProb(word.toLower()) for model in models])
-    # guess = filter(lambda x: models.x.ngramProb(word.toLower()) == highestProb)
-
-    # Return language of model with highest probability
+    highestProb = max(map(lambda x: x.wordProb(word.lower()), self.models))
+    guess = filter(lambda x: x.wordProb(word.lower()) == highestProb, self.models)
     return guess[0].language
 
   def prob(self, language, word):
-    probList = []
-
-    for model in self.models:
-      if model.language == language:
-        probList.append(model)
-
-    prob = probList[0].ngramProb(word.toLower())
-
-    # Functional calls in Python?
-    # prob = filter(lambda x: models.x.language == language)[0].ngramProb(word.toLower())
-
-    return prob
-
+    return filter(lambda x: x.language == language, self.models)[0].wordProb(word.lower())

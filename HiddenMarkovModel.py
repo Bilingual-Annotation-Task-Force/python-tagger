cc7777bd76f616f84a7b59d07b2293fba129c78b
Wally Guzman <gualbertoguzman@gmail.com>
2016-04-28 16:41:37 -0500

    Translating Scala code; working on alternative methods to replicating functional calls, not tested yet

--- HiddenMarkovModel.py
+++ HiddenMarkovModel.py
@@ -13,22 +13,74 @@ class HiddenMarkovModel:
 
   # Run Viterbi algorithm and retrace to compute most likely transitions
   def generateTags(self):
-    viterbi()
-    retrace()
+    self.viterbi()
+    self.retrace()
 
   # Return emission probability
   def em(self, ctx, word):
-    return cslm.prob(ctx, word)
+    return self.cslm.prob(ctx, word)
 
   # Return transmission probability
   def tr(self, ctx, tag):
-    return transitions[ctx][tag]
+    return self.transitions[ctx][tag]
 
   def viterbi(self):
-    return 0.0
+    # Equal probability of starting with either tag
+    for k, tag in enumerate(self.tagSet):
+      self.v[0][k] = Node(math.log(0.5), k)
+
+    for wordIndex, word in enumerate(self.words, start=1):
+      for tagIndex, tag in enumerate(self.tagSet):
+        # Using map
+        # transitionProbs = map(lambda x: Node(self.v[wordIndex - 1][x].prob +
+        #       math.log(self.tr(self.tagSet[x], self.tagSet[tagIndex])), x),
+        #       xrange(len(self.tagSet)))
+
+        # Using list comprehension
+        # transitionProbs = [Node(self.v[wordIndex - 1][x].prob +
+        #   math.log(tr(self.tagSet[x], self.tagSet[tagIndex])), x) for x in
+        #   xrange(len(self.tagSet))]
+
+        # Using loop
+        transitionProbs = []
+        for x, tag in enumerate(self.tagSet):
+          transitionProbs.append(Node(self.v[wordIndex - 1][x].prob +
+            math.log(self.tr(self.tagSet[x], self.tagSet[tagIndex])), x))
+
+
+        maxNode = reduce(lambda n1, n2: n1 if n1.prob > n2.prob else n2, transitionProbs)
+        emissionProb = em(self.tagSet[tagIndex], self.words[wordIndex])
+        self.v[wordIndex][tagIndex] = Node(math.log(emissionProb) +
+            maxNode.prob, maxNode.prevTag)
+        pass
 
   def retrace(self):
-    return 0.0
+    tags = [0 for i in xrange(len(self.words))]
+
+    # Find most probable final tag
+
+    # Using reduce
+    # last = reduce(lambda x, y: x if self.v[len(self.words) - 1][x].prob >
+    #        self.v[len(self.words) - 1][y] else y, xrange(len(self.tagSet)))
+
+    # Using loops
+    last = 0
+    for x, taglist in enumerate(self.tagSet):
+      for y, tag in enumerate(taglist):
+        if self.v[len(self.words) - 1][x].prob > self.v[len(self.words) - 1][y]:
+          last = x
+        else:
+          last = y
+
+    tags[len(self.words) - 1] = self.tagSet[last]
+
+    # Follow backpointers to most probable previous tags
+    prev = self.v[len(self.words) - 1][last].prev
+    for k in xrange(len(self.words) - 2, 0, -1):
+      tags[k] = self.tagSet[prev]
+      prev = self.v[k][prev].prev
+
+    return tags
 
 class Node:
   def __init__(self, prob, prevTag):
